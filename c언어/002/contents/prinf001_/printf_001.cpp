ae822f7bf3c65573d2723fadb2129dcae07bb8eb
leeseung <zxc1728456@gmail.com>
2021-03-11 17:08:28 +0900

    c lang 002

---
+++ c언어/002/contents/prinf001_/printf_001.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+		
+	printf("test c lang");
+	printf("\nwell done ");
+	printf("middle \n in?");
+	printf("end line esacape \n");
+
+
+	return 0;
+}
---
+++ c언어/002/contents/prinf001_/printf_002.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("a \n");
+	printf("%c \n", "a");
+
+	printf("a b c\n");
+	printf("%c %c %c\n", 'a', 'b', 'c');
+	
+	
+	
+	return 0;
+
+}

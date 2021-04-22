341f2a7caa981f712772fabd2dfdbe161e4d0da8
leeseung <zxc1728456@gmail.com>
2021-04-23 05:03:25 +0900

    ALGO

--- ALGO/algotest/algotest/algo_2839.c
+++ ALGO/algotest/algotest/algo_2839.c
@@ -1,20 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-
 int main() {
-	int i, r1 = 0, re = 0, tmp= 0 ;
-	scanf("%d", &i);
+    int i, cnt = 0;
+    scanf("%d", &i);
 
-	re += i / 5;
-	tmp += i % 5;
-	re += tmp / 3;
-	tmp = tmp / 3;
+    for (;1;) {
+        if (i % 5 == 0) {
+            cnt += i / 5;
+            break;
+        }
+        i -= 3;
+        cnt += 1;
+        if (i <= 0) {
+            break;
+        }
+    }
 
-	if (tmp != 0){
-		printf("1");
-		return 0;
-	}
-	printf("%d", re);
-
+    if (i < 0) {
+        printf("-1\n");
+    }
+    else {
+        printf("%d\n", cnt);
+    }
 }
